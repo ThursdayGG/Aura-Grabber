0a099e0ea69d9767e7fe16275ce13072a36d814b
ThursdayGG <131311161+ThursdayGG@users.noreply.github.com>
2023-06-05 22:01:46 -0400

    Add files via upload

---
+++ RobloxPinCracker.py
@@ -0,0 +1,36 @@
+print("""▄▄▄█████▓ ██░ ██  █    ██  ██▀███    ██████ ▓█████▄  ▄▄▄     ▓██   ██▓
+▓  ██▒ ▓▒▓██░ ██▒ ██  ▓██▒▓██ ▒ ██▒▒██    ▒ ▒██▀ ██▌▒████▄    ▒██  ██▒
+▒ ▓██░ ▒░▒██▀▀██░▓██  ▒██░▓██ ░▄█ ▒░ ▓██▄   ░██   █▌▒██  ▀█▄   ▒██ ██░
+░ ▓██▓ ░ ░▓█ ░██ ▓▓█  ░██░▒██▀▀█▄    ▒   ██▒░▓█▄   ▌░██▄▄▄▄██  ░ ▐██▓░
+  ▒██▒ ░ ░▓█▒░██▓▒▒█████▓ ░██▓ ▒██▒▒██████▒▒░▒████▓  ▓█   ▓██▒ ░ ██▒▓░
+  ▒ ░░    ▒ ░░▒░▒░▒▓▒ ▒ ▒ ░ ▒▓ ░▒▓░▒ ▒▓▒ ▒ ░ ▒▒▓  ▒  ▒▒   ▓▒█░  ██▒▒▒ 
+    ░     ▒ ░▒░ ░░░▒░ ░ ░   ░▒ ░ ▒░░ ░▒  ░ ░ ░ ▒  ▒   ▒   ▒▒ ░▓██ ░▒░ 
+  ░       ░  ░░ ░ ░░░ ░ ░   ░░   ░ ░  ░  ░   ░ ░  ░   ░   ▒   ▒ ▒ ░░  
+          ░  ░  ░   ░        ░           ░     ░          ░  ░░ ░     
+                                             ░                ░ ░ """)
+print("BEST ROBLOX PIN CRACKER YOU'LL EVER NEED")
+print("Option 1: No Webhook")
+print("option 2: Webhook")
+print("")
+class main:
+  @staticmethod
+  def ab():
+    print('Enter Cookie: ')
+    choice = input()
+  @staticmethod
+  def bc():
+    print("Enter Webhook: ")
+    choice = input()
+    print('Enter Cookie: ')
+    choice = input()
+choice = input('Please pick your choice: ') #say 1 or 2
+
+if choice == '1':
+  main.ab()
+if choice == '2':
+   main.bc()
+
+import random
+randnumber = random.randint(1493,1493)
+print(f"trying pin",randnumber)
+print(f"Unlocked , pin is",randnumber)
