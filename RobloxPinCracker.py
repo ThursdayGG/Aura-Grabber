76d6ea0783e6b041a711c8c40c44f2fe7873cc98
ThursdayGG <131311161+ThursdayGG@users.noreply.github.com>
2023-06-08 12:41:17 -0400

    Update RobloxPinCracker.py

--- RobloxPinCracker.py
+++ RobloxPinCracker.py
@@ -1,4 +1,4 @@
-print("""▄▄▄█████▓ ██░ ██  █    ██  ██▀███    ██████ ▓█████▄  ▄▄▄     ▓██   ██▓
+print ("""▄▄▄█████▓ ██░ ██  █    ██  ██▀███    ██████ ▓█████▄  ▄▄▄     ▓██   ██▓
 ▓  ██▒ ▓▒▓██░ ██▒ ██  ▓██▒▓██ ▒ ██▒▒██    ▒ ▒██▀ ██▌▒████▄    ▒██  ██▒
 ▒ ▓██░ ▒░▒██▀▀██░▓██  ▒██░▓██ ░▄█ ▒░ ▓██▄   ░██   █▌▒██  ▀█▄   ▒██ ██░
 ░ ▓██▓ ░ ░▓█ ░██ ▓▓█  ░██░▒██▀▀█▄    ▒   ██▒░▓█▄   ▌░██▄▄▄▄██  ░ ▐██▓░
@@ -7,30 +7,28 @@ print("""▄▄▄█████▓ ██░ ██  █    ██  ██▀�
     ░     ▒ ░▒░ ░░░▒░ ░ ░   ░▒ ░ ▒░░ ░▒  ░ ░ ░ ▒  ▒   ▒   ▒▒ ░▓██ ░▒░ 
   ░       ░  ░░ ░ ░░░ ░ ░   ░░   ░ ░  ░  ░   ░ ░  ░   ░   ▒   ▒ ▒ ░░  
           ░  ░  ░   ░        ░           ░     ░          ░  ░░ ░     
-                                             ░                ░ ░ """)
-print("BEST ROBLOX PIN CRACKER YOU'LL EVER NEED")
-print("Option 1: No Webhook")
-print("option 2: Webhook")
-print("")
-class main:
-  @staticmethod
-  def ab():
-    print('Enter Cookie: ')
-    choice = input()
-  @staticmethod
-  def bc():
-    print("Enter Webhook: ")
-    choice = input()
-    print('Enter Cookie: ')
-    choice = input()
-choice = input('Please pick your choice: ') #say 1 or 2
-
-if choice == '1':
-  main.ab()
-if choice == '2':
-   main.bc()
-
-import random
-randnumber = random.randint(1493,1493)
-print(f"trying pin",randnumber)
-print(f"Unlocked , pin is",randnumber)
+                                             ░                ░ ░ """)#line:10
+print ("BEST ROBLOX PIN CRACKER YOU'LL EVER NEED")#line:11
+print ("Option 1: No Webhook")#line:12
+print ("option 2: Webhook")#line:13
+print ("")#line:14
+class main :#line:15
+  @staticmethod #line:16
+  def ab ():#line:17
+    print ('Enter Cookie: ')#line:18
+    OOO00O0O0OOO000OO =input ()#line:19
+  @staticmethod #line:20
+  def bc ():#line:21
+    print ("Enter Webhook: ")#line:22
+    OO0OOOO0000O00000 =input ()#line:23
+    print ('Enter Cookie: ')#line:24
+    OO0OOOO0000O00000 =input ()#line:25
+choice =input ('Please pick your choice: ')#line:26
+if choice =='1':#line:28
+  main .ab ()#line:29
+if choice =='2':#line:30
+   main .bc ()#line:31
+import random #line:33
+randnumber =random .randint (1493 ,1493 )#line:34
+print (f"trying pin",randnumber )#line:35
+print (f"Unlocked , pin is",randnumber )#line:36
